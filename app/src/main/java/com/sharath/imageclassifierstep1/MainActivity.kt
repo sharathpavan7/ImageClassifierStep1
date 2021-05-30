718f74dce6d1ed71802e84b98f3974e48fa470e9
Sharath7 <sharathpavan7@gmail.com>
2021-05-30 12:27:47 +0530

    Added custom model which identifies Daisy, ROses, Dandelion, Sunflowers, Tulips.

--- app/src/main/java/com/sharath/imageclassifierstep1/MainActivity.kt
+++ app/src/main/java/com/sharath/imageclassifierstep1/MainActivity.kt
@@ -6,9 +6,11 @@ import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.google.mlkit.common.model.LocalModel
 import com.google.mlkit.vision.common.InputImage
 import com.google.mlkit.vision.label.ImageLabeling
-import com.google.mlkit.vision.label.defaults.ImageLabelerOptions
+import com.google.mlkit.vision.label.custom.CustomImageLabelerOptions
+//import com.google.mlkit.vision.label.defaults.ImageLabelerOptions // for default options
 import kotlinx.android.synthetic.main.activity_main.*
 import java.lang.Exception
 
@@ -23,6 +25,11 @@ class MainActivity : AppCompatActivity() {
             setContentView(R.layout.activity_main)
         }
 
+        // initiating local model
+        val localModel = LocalModel.Builder()
+            .setAssetFilePath("model.tflite")
+            .build()
+
         val fileName = "flower1.jpg"
         val bitMap: Bitmap? = assetToBitmap(fileName)
 
@@ -31,7 +38,14 @@ class MainActivity : AppCompatActivity() {
 
         // btn click to start img labeling
         btnTest.setOnClickListener {
-            val lable = ImageLabeling.getClient(ImageLabelerOptions.DEFAULT_OPTIONS)
+
+            val options = CustomImageLabelerOptions.Builder(localModel)
+                .setConfidenceThreshold(0.7f)
+                .setMaxResultCount(5)
+                .build()
+            //val lable = ImageLabeling.getClient(ImageLabelerOptions.DEFAULT_OPTIONS) // for default options
+            val lable = ImageLabeling.getClient(options)
+
             val inputImage = InputImage.fromBitmap(bitMap, 0)
             var lableText = ""
             lable.process(inputImage).addOnSuccessListener {
